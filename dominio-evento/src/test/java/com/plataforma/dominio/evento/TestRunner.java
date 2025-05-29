91d4420675234e7fc8b019b1ef0c4df8bd84d77c
felipesergiob <felipessmbf@gmail.com>
2025-05-28 23:04:25 -0300

    refactor: substitui CucumberTestRunner por TestRunner com melhorias na configuração de testes BDD

--- dominio-evento/src/test/java/com/plataforma/dominio/evento/TestRunner.java
+++ dominio-evento/src/test/java/com/plataforma/dominio/evento/TestRunner.java
@@ -6,13 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/com/plataforma/dominio/evento",
+    features = "src/test/resources/com/plataforma/dominio/evento/features",
     glue = "com.plataforma.dominio.evento",
     plugin = {
         "pretty",
         "html:target/cucumber-reports/cucumber-pretty.html",
-        "json:target/cucumber-reports/CucumberTestReport.json"
-    }
+        "json:target/cucumber-reports/CucumberTestReport.json",
+        "junit:target/cucumber-reports/CucumberTestReport.xml"
+    },
+    monochrome = true,
+    tags = "not @ignorar"
 )
-public class CucumberTestRunner {
-}
+public class TestRunner {
+} 
